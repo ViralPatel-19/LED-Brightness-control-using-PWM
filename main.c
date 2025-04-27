52f6b5fd1a1684e73f6f2e4ff55f7d22922fc0dc
VIRAL PATEL <viral19patel.vp@gmail.com>
2025-04-27 15:30:57 +0530

    LED brightness control using PWM method

---
+++ main.c
@@ -0,0 +1,125 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for Hello World Example using HAL APIs.
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* Copyright 2022-2024, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+
+
+
+int main(void)
+{
+    cy_rslt_t result;
+
+
+#if defined (CY_DEVICE_SECURE)
+    cyhal_wdt_t wdt_obj;
+
+    /* Clear watchdog timer so that it doesn't trigger a reset */
+    result = cyhal_wdt_init(&wdt_obj, cyhal_wdt_get_max_timeout_ms());
+    CY_ASSERT(CY_RSLT_SUCCESS == result);
+    cyhal_wdt_free(&wdt_obj);
+#endif /* #if defined (CY_DEVICE_SECURE) */
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init();
+
+    /* Board init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    /* Initialize retarget-io to use the debug UART port */
+    result = cy_retarget_io_init_fc(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX,
+            CYBSP_DEBUG_UART_CTS,CYBSP_DEBUG_UART_RTS,CY_RETARGET_IO_BAUDRATE);
+
+    /* retarget-io init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+    /* Initialize the counter in the TCPWM block for the PWM operation.*/
+
+
+    if (CY_TCPWM_SUCCESS != Cy_TCPWM_PWM_Init(TCPWM1, 14UL, &tcpwm_1_cnt_14_config))
+       {
+    	CY_ASSERT(0);
+       }
+
+
+     /* Enable the TCPWM for PWM mode of operation */
+
+     Cy_TCPWM_PWM_Enable(TCPWM1, 14UL);
+
+     /* Start the TCPWM block */
+
+     Cy_TCPWM_TriggerStart_Single(TCPWM1, 14UL);
+
+     Cy_GPIO_Pin_Init(CYBSP_LED8_PORT, CYBSP_LED8_PIN, &CYBSP_LED8_config);
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
+    printf("\x1b[2J\x1b[;H");
+
+    printf("****************** "
+           "ADC read data example "
+           "****************** \r\n\n");
+
+
+
+
+    for (;;)
+    {
+
+    	for(int i = 0; i < 100; i++){
+    				printf("  %d  ",i);
+    	    		Cy_TCPWM_PWM_SetCompare0(TCPWM1, 14UL, i);
+    	    		Cy_SysLib_Delay(100); // Delay 10 ms
+    	    	}
+
+    }
+}
+
+
+
+/* [] END OF FILE */
